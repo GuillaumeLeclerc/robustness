4eb8c81b2aa2157ea7a84a3341a1aaae9e21c8c4
Guillaume Leclerc <leclerc@mit.edu>
2021-04-20 17:18:29 -0400

    Faster data loading

--- robustness/data/datasets/from_webdataset_generator.py
+++ robustness/data/datasets/from_webdataset_generator.py
@@ -11,6 +11,9 @@ from .dataset_factory import DatasetFactory
 from .device_copier import DeviceCopier
 from ..augmentation.augmenter import Augmenter, apply_augmenter
 
+def worker_init(self):
+    ch.set_num_threads(1)
+
 def custom_collation_fn(samples, combine_tensors=True, combine_scalars=True):
     """Take a collection of samples (dictionaries) and create a batch.
     If `tensors` is True, `ndarray` objects are combined into
@@ -41,6 +44,52 @@ def custom_collation_fn(samples, combine_tensors=True, combine_scalars=True):
     return result
 
 
+class EpochSeparator:
+
+    def __init__(self, dataset):
+        self.it = iter(dataset)
+        self.epoch = -1
+        self.value = None
+
+    def __iter__(self):
+        self.epoch += 1
+        return self
+
+    def __next__(self):
+        result = None
+        if self.value is not None:
+            result = self.value
+            self.value = None
+        else:
+            result = next(self.it)
+
+        epoch, data = result
+        if epoch > self.epoch:
+            self.value = result
+            raise StopIteration()
+        else:
+            return data
+
+class EpochAdder(IterableDataset):
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.epoch = 0
+        self.it = iter(dataset)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            n = next(self.it)
+            return self.epoch, n
+        except StopIteration:
+            self.epoch += 1
+            self.it = iter(self.dataset)
+            return next(self)
+
+
 class FromWebDatasetFactory(DatasetFactory, metaclass=ABCMeta):
 
     def __init__(self, shards):
@@ -49,27 +98,31 @@ class FromWebDatasetFactory(DatasetFactory, metaclass=ABCMeta):
     def generate_sample_dataset(self, augmenter: Augmenter = None,
                  shuffle_size: int = 0) -> IterableDataset:
         dataset = wds.WebDataset(self.shards, shardshuffle=shuffle_size > 0)
+
+        dataset = dataset.then(wds.iterators.shuffle, shuffle_size,
+                               shuffle_size)
+
         dataset = self.decode(dataset)
-        if shuffle_size > 1:
-            dataset = dataset.then(wds.iterators.shuffle, shuffle_size,
-                                   shuffle_size)
-        if augmenter:
-            dataset = dataset.map(partial(apply_augmenter, factory=self,
-                                          transform=augmenter.transform_sample_data,
-                                          transform_label=augmenter.transform_sample_label))
+
         return dataset
+
 
     def generate_batched_dataset(self, batch_size, num_workers=0,
                                  augmenter: Augmenter = None,
                                  shuffle_size: int = 0, pin_memory=True,
                                  device='cpu'):
+        num_workers = min(num_workers, len(self.shards))
         dataset = self.generate_sample_dataset(augmenter, shuffle_size)
         dataset = dataset.then(wds.iterators.batched, batchsize=batch_size, collation_fn=custom_collation_fn)
+        dataset = EpochAdder(dataset)
 
         dataset = DataLoader(dataset, batch_size=None, num_workers=num_workers,
-                             pin_memory=pin_memory)
+                             pin_memory=pin_memory, worker_init_fn=worker_init,
+                             shuffle=False, sampler=None)
 
-        return DeviceCopier(dataset, device, self, augmenter)
+        dataset = EpochSeparator(dataset)
+        dataset = DeviceCopier(dataset, device, self, augmenter)
+        return dataset
 
     @abstractmethod
     def normalizer(self):
